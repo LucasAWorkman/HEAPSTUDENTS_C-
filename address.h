552ac4c02609d5e37a80abefa7d21865783ac696
LucasAWorkman <lucas.workman@bsu.edu>
2025-02-28 17:51:07 +0000

    .

---
+++ address.h
@@ -0,0 +1,22 @@
+#define indef
+
+#include <iostream>
+
+class Address() {
+  public:
+    string[] street;
+    string[] city;
+    string[] state;
+    string[] zipcode;
+  private:
+   void Address(Construtor -> string[]) {
+	   return street, city, state, zipcode -> string[]
+    }
+
+  void Constructor(city -> string[], state -> string[], zipcode -> const static int, street -> string[]){
+	 return Constructor()
+    }
+
+  return 0;
+
+} 
---
+++ date.h
@@ -0,0 +1,25 @@
+#define indef 
+
+#include <iostream>
+
+class Date(string[] time) {
+  public:
+    string[] time;
+  private:
+
+    // constructor
+    Date(string[] time = time) {
+	  return time;
+    }
+
+    Date.init(time: string) {
+	  return Date(time)
+    }
+    
+    Date.printDate() {
+	    std::cout << time << endl;
+  }
+
+}
+
+#end indef
